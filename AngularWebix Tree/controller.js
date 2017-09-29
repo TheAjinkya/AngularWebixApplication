c0b033655b242ef18aeb58ddc6c9059992325c0a
ajinkya.chanshetty <ajinkya.chanshetty@gmail.com>
2017-09-29 13:11:19 +0530

    Webix DataTable from external Server

--- AngularWebix Tree/controller.js
+++ AngularWebix Tree/controller.js
@@ -5,75 +5,21 @@
 
             })
 
-            app.controller('treeController', function($scope){
+            app.controller('treeController', function($scope, $http){
+                
+                $http.get('data.json').then(function(response){
+                    
+                    $scope.content = response.data.treeData;
+                })
+                
+             
+                
 
                 $scope.tree_config = {
                     view : 'tree',
                     id : 'testTree',
                     link :"",
                     
-                    data:[
-                        {
-                            value : 'One',
-                            webix_kids : false,
-                            id : '1',
-                            link:"about",
-                            
-                        },
-                        {
-                            value : 'Two',
-                            webix_kids : false,
-                            id : '2',
-                            /*template: '<a ui-sref="#value#" href="##value#">#value#</a>',*/
-                            data:[
-                                { id:"1.2.1", value:"Page 1" },
-                                { id:"1.2.2", value:"Page 2" },
-                                { id:"1.2.3", value:"Page 3" },
-                                { id:"1.2.4", value:"Page 4" },
-                                { id:"1.2.5", value:"Page 5" } ]
-                        },
-                        {
-                            value : 'Two',
-                            webix_kids : false,
-                            id : '2',
-                            /*template: '<a ui-sref="#value#" href="##value#">#value#</a>',*/
-                            data:[
-                                { id:"1.2.1", value:"Page 1" },
-                                { id:"1.2.2", value:"Page 2" },
-                                { id:"1.2.3", value:"Page 3" },
-                                { id:"1.2.4", value:"Page 4" },
-                                { id:"1.2.5", value:"Page 5" } ]
-                        },
-                        {
-                            value : 'Two',
-                            webix_kids : false,
-                            id : '2',
-                            /*template: '<a ui-sref="#value#" href="##value#">#value#</a>',*/
-                            data:[
-                                { id:"1.2.1", value:"Page 1" },
-                                { id:"1.2.2", value:"Page 2" },
-                                { id:"1.2.3", value:"Page 3" },
-                                { id:"1.2.4", value:"Page 4" },
-                                { id:"1.2.5", value:"Page 5" } ]
-                        },
-                        {
-                            value : 'Two',
-                            webix_kids : false,
-                            id : '2',
-                            /*template: '<a ui-sref="#value#" href="##value#">#value#</a>',*/
-                            data:[
-                                { id:"1.2.1", value:"Page 1" },
-                                { id:"1.2.2", value:"Page 2" },
-                                { id:"1.2.3", value:"Page 3" },
-                                { id:"1.2.4", value:"Page 4" },
-                                { id:"1.2.5", value:"Page 5" } ]
-                        },
-                        {
-                            value : 'Three',
-                            webix_kids : false,
-                            id : '3',
-                           /* template: '<a ui-sref="#value#" href="##value#>#value#</a>'*/
-                        }
-                    ]
+                    data:angular.toJson($scope.content)
                 }  
             })
