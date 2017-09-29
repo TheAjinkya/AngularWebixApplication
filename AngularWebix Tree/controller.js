ce0d6a0aacb86804392eb4f49a609339be2e726d
ajinkya.chanshetty <ajinkya.chanshetty@gmail.com>
2017-09-29 17:37:19 +0530

    Additional webix components

--- AngularWebix Tree/controller.js
+++ AngularWebix Tree/controller.js
@@ -1,25 +1,26 @@
-            app.controller('addCtrl', function($scope){
+app.controller('addCtrl', function($scope){
 
-                $scope.firstNum = 885;
-                $scope.secondNum = 995;
+    $scope.firstNum = 885;
+    $scope.secondNum = 995;
 
-            })
+})
 
-            app.controller('treeController', function($scope, $http){
-                
-                $http.get('data.json').then(function(response){
-                    
-                    $scope.content = response.data.treeData;
-                })
-                
-             
-                
+app.controller('treeController', function($scope, $http){
 
-                $scope.tree_config = {
-                    view : 'tree',
-                    id : 'testTree',
-                    link :"",
-                    
-                    data:angular.toJson($scope.content)
-                }  
-            })
+    $http.get('data.json').then(function(response){
+
+        var content = response.data
+    })
+
+
+
+
+    $scope.tree_config = {
+        view : 'tree',
+        id : 'testTree',
+        autoheight:true,
+        autowidth:true,
+        
+        
+    }  
+})
