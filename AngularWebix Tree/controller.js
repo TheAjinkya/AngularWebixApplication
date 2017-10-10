cd5979ffd35c95d71ec8f3a1b1bef47aaa0ac23f
ajinkya.chanshetty <ajinkya.chanshetty@gmail.com>
2017-10-10 12:59:48 +0530

    Changes

--- AngularWebix Tree/controller.js
+++ AngularWebix Tree/controller.js
@@ -7,20 +7,89 @@ app.controller('addCtrl', function($scope){
 
 app.controller('treeController', function($scope, $http){
 
-    $http.get('data.json').then(function(response){
+    //    $http.get('data.json').then(function(response){
+    //        
+    //        $scope.content = response.data.treeData
+    //        
+    //    })
 
-        var content = response.data
-    })
+
+
+    treeData : [
+        {
+            value : 'One',
+            webix_kids : false,
+            id : '1',
+            link :'about'
+
+        },
+        {
+            value : 'Two',
+            webix_kids : false,
+            id : '2',
+
+            data:[
+                { id:"1.2.1", value:"Page 1" },
+                { id:"1.2.2", value:"Page 2" },
+                { id:"1.2.3", value:"Page 3" },
+                { id:"1.2.4", value:"Page 4" },
+                { id:"1.2.5", value:"Page 5" } ]
+        },
+        {
+            value : 'Two',
+            webix_kids : false,
+            id : '2',
+
+            data:[
+                { id:"1.2.1", value:"Page 1" },
+                { id:"1.2.2", value:"Page 2" },
+                { id:"1.2.3", value:"Page 3" },
+                { id:"1.2.4", value:"Page 4" },
+                { id:"1.2.5", value:"Page 5" } ]
+        },
+        {
+            value : 'Two',
+            webix_kids : false,
+            id : '2',
+
+            data:[
+                { id:"1.2.1", value:"Page 1" },
+                { id:"1.2.2", value:"Page 2" },
+                { id:"1.2.3", value:"Page 3" },
+                { id:"1.2.4", value:"Page 4" },
+                { id:"1.2.5", value:"Page 5" } ]
+        },
+        {
+            value : 'Two',
+            webix_kids : false,
+            id : '2',
+
+            data:[
+                { id:"1.2.1", value:"Page 1" },
+                { id:"1.2.2", value:"Page 2" },
+                { id:"1.2.3", value:"Page 3" },
+                { id:"1.2.4", value:"Page 4" },
+                { id:"1.2.5", value:"Page 5" } ]
+        },
+        {
+            value : 'Three',
+            webix_kids : false,
+            id : '3'
+
+        }
+    ]
 
 
 
-
-    $scope.tree_config = {
+    $scope.treeConfig = {
+        
         view : 'tree',
         id : 'testTree',
         autoheight:true,
         autowidth:true,
-        
-        
+        data : 'treeData'
+
+
+
     }  
 })
